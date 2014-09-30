6bb664431c5ece8965c49231ce337b8c3f6ea88a
Tim Van <hoangvd241@gmail.com>
2014-09-30 23:40:02 +1000

    k

--- Tetris.Test/GameSpec.js
+++ Tetris.Test/GameSpec.js
@@ -9,9 +9,17 @@ define(['../Tetris/Game'], function (Game)
 		{
 			var game = new Game(2, 2);
 			expect(game.isGameOver()).toBe(false);
-			game.board[1][0] = 1;
 			game.board[1][1] = 1;
+			game.board[1][0] = 1;
 			expect(game.isGameOver()).toBe(true);
+		});
+
+		it("removeFilledLines", function ()
+		{
+			var game = new Game(5, 3);
+			game.board = [[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 1], [1, 1, 1]];
+			game.removeFilledLines();
+			expect(game.board).toEqual([[0, 0, 0], [0, 0, 0], [0, 0, 0], [1, 0, 0], [0, 1, 0]]);
 		});
 	});
 });
--- Tetris/Game.js
+++ Tetris/Game.js
@@ -5,9 +5,6 @@ function ()
 {
 	function Game(width, length)
 	{
-		this.width = width;
-		this.length = length;
-
 		this.board = new Array(length);
 		for (var i = 0; i < length; i++)
 		{
@@ -19,18 +16,85 @@ function ()
 		}
 	}
 
-	//Game.
-
-	Game.prototype.isGameOver = function ()
+	Game.prototype.removeFilledLines = function ()
 	{
-		for (var i = 0; i < this.width; i++)
+		var resetBottomRows = function(array, startRow)
 		{
-			if (this.board[this.length - 1][i] == 0)
+			for(var i = startRow; i < array.length; i++)
+			{
+				for(var j = 0; j < array[0].length; j++)
+				{
+					array[i][j] = 0;
+				}
+			}
+		}
+
+		var moveRowsToBottom = function(array, startRow, endRow)
+		{
+			for(var i = endRow; i >= startRow; i--)
+			{
+				var idx = board.length - 1 - endRow + i;
+				var tmp = array[idx];
+				array[idx] = array[i];
+				array[i] = tmp;
+			}
+		}
+
+		var board = this.board;
+
+		var lastIndexOfContinuousFilledLine = board.length;
+		while (lastIndexOfContinuousFilledLine - 1 > 0 && isLineFilled(board[lastIndexOfContinuousFilledLine - 1]))
+		{
+			lastIndexOfContinuousFilledLine--;
+		}
+
+		resetBottomRows(this.board, lastIndexOfContinuousFilledLine);
+
+		if (lastIndexOfContinuousFilledLine != board.length)
+		{
+			for (var i = board.length - 1 ; i > 0 ; i--)
+			{
+				if (i >= lastIndexOfContinuousFilledLine)
+				{
+					board[i] = board[lastIndexOfContinuousFilledLine - (board.length - 1 - i)];
+				}
+				else
+				{
+					board[i] = new Array(board[0].length);
+					for (var j = 0; j++; j < board[0].length)
+					{
+						board[i][j] = 0;
+					}
+				}
+			}
+		}
+	};
+
+	function doesRowOnlyContainValue(row, value)
+	{
+		for (var i = 0; i < row.length; i++)
+		{
+			if (row[i] !== value)
 			{
 				return false;
 			}
 		}
 		return true;
+	}
+
+	function isEmptyRow(row)
+	{
+		return doesRowOnlyContainValue(row, 0);
+	}
+
+	function isFilledRow(row)
+	{
+		return doesRowOnlyContainValue(row, 1);
+	}
+
+	Game.prototype.isGameOver = function ()
+	{
+		return !isEmptyRow(this.board[0]);
 	};
 
 	return Game;
